7d073f2093173aad7152ed3037f176216d4f3498
Kushal Shah <kushal@pdx.edu>
2018-02-21 09:49:10 -0800

    Edited Package with error solved and some places commented as CONFUSION

--- rtl/AHB_Package.sv
+++ rtl/AHB_Package.sv
@@ -12,67 +12,65 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
-`ifndef DEFS
-	'define DEFS
-
-
 package Definitions;
 
 parameter DATAWIDTH       = 32;
 parameter ADDRWIDTH       = 32;
 parameter TRANS_WIDTH     = 2;
-parameter BURST_SIZE = 3;
-parameter BURST_TYPE = 3;
+parameter BURST_SIZE      = 3;
+parameter BURST_TYPE      = 3;
+
+logic HSEL0, HSEL1, MUX_SEL;                       // Confusion for this line existance
+logic HRESETn, HCLK;                               // Confusion for this line existance
 
 //---------------------------------------Tranfer types' enumeration------------------------------------------
 
-logic HSEL0, HSEL1, MUX_SEL,
-
-logic HRESETn, HCLK;
-
-typedef enum logic [TRANS_WIDTH-1:0] {
+typedef enum logic [TRANS_WIDTH-1:0] 
+{
 IDLE   = 2'b00,
 BUSY   = 2'b01,
 NONSEQ = 2'b10,
-SEQ    = 2'b11 } Trans_t;
+SEQ    = 2'b11 
+} Trans_t;
 
 
-endpackage
-//---------------------------STRUCTURE for Address and Data------------------------------------
-typedef struct  Instruction 
+//---------------------------------------STRUCTURE for Address and Data--------------------------------------
+
+typedef struct  
 {
- logic[ADDRWIDTH-1:0]	HADDR;
- logic[DATAWIDTH-1:0]	HWDATA; }Instruction_t;
-//----------------------------Signal for SLave---------------------------------------
-typedef struct Slave
+ logic [ADDRWIDTH-1:0]	HADDR;
+ logic [DATAWIDTH-1:0]	HWDATA; 
+} Instruction_t;
+
+
+//---------------------------------------Signal for Slave----------------------------------------------------
+
+typedef struct 
 {
- logic[DATAWIDTH-1:0] HRDATA;
- logic HRESP;
- logic HREADYOUT; 
+ logic [DATAWIDTH-1:0] HRDATA;
+ logic                 HRESP;
+ logic                 HREADYOUT; 
 } Slave_t;
 
-Slave_t slave_0,slave_1,mux_out;
---------------------------Structure for Master o/p signal--------------------------
-struct Master
+Slave_t slave_0, slave_1, mux_out;
+
+
+//--------------------------------------Structure for Master o/p signal--------------------------------------
+struct 
 {
- Instruction_t HADDR;
- Instruction_t HWDATA;
- logic HWRITE;
- logic HMASTLOCK;
- logic HREADY;
- Trans_t [TRANS_WIDTH-1:0] HTRANS;
- logic [BURST_SIZE-1:0] HSIZE;
- logic [BURST_TYPE-1:0] HBURST;
- logic [3:0] HPROT;
+ Instruction_t HADDR;                                // Confusion
+ Instruction_t HWDATA;                               // Confusion
+ logic         HWRITE;
+ logic         HMASTLOCK;
+ logic         HREADY;
+ Trans_t       [TRANS_WIDTH-1:0] HTRANS;
+ logic         [BURST_SIZE-1:0]  HSIZE;
+ logic         [BURST_TYPE-1:0]  HBURST;
+ logic         [3:0]             HPROT;
 } Master_t;
--------------------------------------------------------------------------
+
  
  
+endpackage
 
 
-
-
-
-import Definitions ::*;
-`endif
